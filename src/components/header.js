53a13980132a06633a99179fb3fe20caf4d5e795
Jason Cochran <jlcochran2013@gmail.com>
2017-04-13 12:06:33 -0500

    updated header and footer

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,11 @@ const Header = () => {
     <header>
       <h2></h2>
   		<nav>
-        <li><a href="#background">Background</a></li>
-        <li><a href="#resume">Resume</a></li>
-        <li><a href="#hire-me">Hire Me</a></li>
+        <li><a href='#background'>Background</a></li>
+        <li><a href='#resume'>Resume</a></li>
+        <li><a href='#hire-me'>Hire Me</a></li>
+        <li><a href='https://twitter.com/app_jitsu' target='_blank'>Twitter</a></li>
+        <li><a href='https://github.com/appjitsu' target='_blank'>GitHub</a></li>
   		</nav>
   	</header>
   )
