5aca1aa3e83a37d79cbbd33209d5dbe0c8e7bd9e
Will Xiong <will_xiong@hotmail.com>
2018-07-29 14:46:54 -0700

    Adjustments & Footer v1

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer>
       <a href='https://github.com/appjitsu/jasonleecochran_com' target='_blank'><img className='fork' src={`${process.env.PUBLIC_URL}/img/fork.png`} alt='' /></a>
       <img className='logos' src={`${process.env.PUBLIC_URL}/img/logos.png`} alt='' />
-      <p className="copyright">&copy; {year} jasonleecochran.com. All rights reserved.</p>
+      <p className="copyright">&copy; {year} willXiong.com. All rights reserved.</p>
   	</footer>
   )
 }
